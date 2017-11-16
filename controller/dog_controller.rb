bb64ac43bcd8d99b6262746aebd910ff00d566f5
NRothera <31472463+NRothera@users.noreply.github.com>
2017-11-16 20:45:40 +0000

    angry dogs edit link works

--- controller/dog_controller.rb
+++ controller/dog_controller.rb
@@ -12,13 +12,27 @@ class DogController < Sinatra::Base
 	 title: "Cute pupper",
 	 body: "Pupper stares whimzically into the distance",
    image: "http://img.zergnet.com/731737_300.jpg"
-},
-{
+   },
+   {
     id: 1,
     title: "Fluffy pupper",
     body: "Souless dark eyes stare up at you",
     image: "http://media.dogaidsociety.com.s3-eu-west-1.amazonaws.com/IMG_0253-300x300.jpg"
-}]
+  }]
+
+  $angry_dogs = [{
+    id:0,
+    title: "ohh nooo he's angry",
+    body: "He's going to get you!",
+    image: "http://doggroomeradvice.com/wp-content/uploads/2012/03/angry-dog-300x300.jpg"
+    },
+    {
+    id: 1,
+    title: "This one's upset too!",
+    body: "better give him a treat",
+    image: "https://i.pinimg.com/736x/15/1f/47/151f470bcc9e668264d71460fdb872cf--funny-humor-funny-dogs.jpg"
+      }]
+
 
   get "/" do
     @title = "Dog Homepage"
@@ -40,6 +54,7 @@ class DogController < Sinatra::Base
 
   get "/dogs/angry" do
     @title = "Angry dogs"
+    @angry_dog = $angry_dogs
     erb :'angry_dogs/index'
 
   end
@@ -47,6 +62,13 @@ class DogController < Sinatra::Base
   get "/dogs/angry/new" do
     @title = "New Dog"
     erb :'angry_dogs/new'
+  end
+
+  get "/angry_dogs/:id" do
+    id = params[:id].to_i
+
+    @angry_dog = $angry_dogs[id]
+    erb :'angry_dogs/show'
   end
 
   post "/dogs" do
