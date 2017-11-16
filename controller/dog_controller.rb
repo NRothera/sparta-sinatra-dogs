30beb37d3e6e60a3e27ac03d22478f9d5fb3256d
NRothera <31472463+NRothera@users.noreply.github.com>
2017-11-16 20:17:29 +0000

    connected angry dogs page

--- controller/dog_controller.rb
+++ controller/dog_controller.rb
@@ -9,20 +9,15 @@ class DogController < Sinatra::Base
 
   $dogs = [{
 	 id: 0,
-	 title: "Dog 1",
-	 body: "This is the first Dog",
+	 title: "Cute pupper",
+	 body: "Pupper stares whimzically into the distance",
    image: "http://img.zergnet.com/731737_300.jpg"
 },
 {
     id: 1,
-    title: "Dog 2",
-    body: "This is the second dog",
+    title: "Fluffy pupper",
+    body: "Souless dark eyes stare up at you",
     image: "http://media.dogaidsociety.com.s3-eu-west-1.amazonaws.com/IMG_0253-300x300.jpg"
-},
-{
-    id: 2,
-    title: "Click here for an angry dog",
-    body: "Angry dog",
 }]
 
   get "/" do
@@ -47,6 +42,11 @@ class DogController < Sinatra::Base
     @title = "Angry dogs"
     erb :'angry_dogs/index'
 
+  end
+
+  get "/dogs/angry/new" do
+    @title = "New Dog"
+    erb :'angry_dogs/new'
   end
 
   post "/dogs" do
